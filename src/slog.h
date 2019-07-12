9063317dbd19413537dc2ccd36721e9eb44c8f71
zhaohailong <zhaohailong@digisky.com>
2019-07-12 11:31:26 +0800

    add

--- src/slog.h
+++ src/slog.h
@@ -6,31 +6,6 @@
 #include <stdio.h>
 #include <string.h>
 
-void snprintf_s(char*buf, int _max, int& len, const char* fmt, ...)
-{
-    if (len >= 0 && len < _max)
-    {
-        va_list arg;
-        va_start(arg, fmt);
-        len += vsnprintf(buf + len, _max - len, fmt, arg);
-        va_end(arg);
-    }
-    buf[_max - 1] = 0;
-}
-
-void Log( const char* file, const char* fun, int line, const char* msg, ...)
-{
-    va_list arg_ptr;
-    va_start(arg_ptr, msg);
-    char buf[1024];
-    int len = 0;
-    memset(&buf[0], 0, sizeof(buf));
-    vsnprintf(buf, sizeof(buf), msg, arg_ptr);
-
-    char final_buf[2048];
-    snprintf_s(final_buf, sizeof(final_buf), len, 
-        "[%lu]%s %s %d %s\n",  pthread_self(), file, fun, line, buf);
-    printf("%s\n", final_buf);
-}
+void Log(const char *file, const char *fun, int line, const char *msg, ...);
 #define LOG(msg, ...)     Log(__FILE__, __FUNCTION__, __LINE__, msg, ## __VA_ARGS__)
 #endif
