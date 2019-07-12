9ddcdd9fbcfd31f79e99bcd427f6661aaf567cd9
zhaohailong <zhaohailong@digisky.com>
2019-07-12 11:22:50 +0800

    add

--- src/slog.h
+++ src/slog.h
@@ -1,5 +1,21 @@
 #ifndef slog_h
 #define slog_h
+#include <sys/io.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+void snprintf_s(char*buf, int _max, int& len, const char* fmt, ...)
+{
+    if (len >= 0 && len < _max)
+    {
+        va_list arg;
+        va_start(arg, fmt);
+        len += vsnprintf(buf + len, _max - len, fmt, arg);
+        va_end(arg);
+    }
+    buf[_max - 1] = 0;
+}
+
 void Log( const char* file, const char* fun, int line, const char* msg, ...)
 {
     va_list arg_ptr;
